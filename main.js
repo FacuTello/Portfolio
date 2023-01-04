bd8afffeb54e3afe1cd3a7de11c7f7ddd97de95f
FacuTello <facundostello@hotmail.com>
2023-01-04 17:20:58 -0300

    projects-section advance

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-const soy = document.querySelector('#soy');
-const facundo = document.querySelector('#facundo');
-const front = document.querySelector('#front');
-const developer = document.querySelector('#developer');
 const button = document.querySelector('#button');
+const button2 = document.querySelector('#button2');
+const originalPresentation = document.querySelector('.presentation-container');
+const modifiedPresentation = document.querySelector('.second-presentation-container');
+
+button.addEventListener('click', cambiarPortada);
+button2.addEventListener('click', cambiarPortada);
+
 
 function cambiarPortada(){
-    soy.textContent = "Sobre mi";
-    facundo.textContent = "Proyectos";
-    front.textContent = "Habilidades";
-    developer.textContent = "Contacto";
+    if (modifiedPresentation.classList.contains('desactive')){
+        originalPresentation.classList.add('desactive');
+        modifiedPresentation.classList.remove('desactive');
+    } else {
+        originalPresentation.classList.remove('desactive');
+        modifiedPresentation.classList.add('desactive');
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
