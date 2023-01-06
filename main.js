3644b4cdbb7a7749a2efd2a1ba408cc839b314b4
FacuTello <facundostello@hotmail.com>
2023-01-06 18:03:28 -0300

    feat: Section Projects

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const button2 = document.querySelector('#button2');
 const originalPresentation = document.querySelector('.presentation-container');
 const modifiedPresentation = document.querySelector('.second-presentation-container');
 
-button.addEventListener('click', cambiarPortada);
-button2.addEventListener('click', cambiarPortada);
+button.addEventListener('click', changeFront);
+button2.addEventListener('click', changeFront);
 
 
-function cambiarPortada(){
+function changeFront(){
     if (modifiedPresentation.classList.contains('desactive')){
         originalPresentation.classList.add('desactive');
         modifiedPresentation.classList.remove('desactive');
@@ -16,6 +16,31 @@ function cambiarPortada(){
         modifiedPresentation.classList.add('desactive');
     }
 }
+
+const linkProjects = document.querySelector('#project-link');
+const sectionProjects = document.querySelector('#sectionProjects');
+linkProjects.addEventListener('click', projectsToggle);
+
+function projectsToggle(){
+    if (sectionProjects.classList.contains('desactive')){
+        modifiedPresentation.classList.add('desactive');
+        sectionProjects.classList.add('work-section');
+    }
+}
+
+const backButtonOfProjects = document.querySelector('#backButtonOfProjects');
+backButtonOfProjects.addEventListener('click', backMain);
+
+function backMain(){
+     if(sectionProjects.classList.contains('work-section')){
+        sectionProjects.classList.replace('work-section', 'desactive');
+        modifiedPresentation.classList.replace('desactive', 'second-presentation-container');
+     }
+}
+
+
+
+
 
 
 
